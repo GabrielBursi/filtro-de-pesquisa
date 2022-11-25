6e81d8c401233280f75d5054dd7b7298ef0a5981
Gabriel Bursi <gabrielcavalarosuporte@gmail.com>
2022-11-25 16:17:15 -0300

    função para filtrar

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 // get filter element
+const filter = document.querySelector('#filter')
 // get cards elements
+const cards = document.querySelectorAll('li')
 
 // add input event for the filter element
+filter.addEventListener('input',filterCard)
 
 // filter function
-// if the filter is not empty
-// for each card of cards
-// get card heading (title)
-// tranform to lower case
-// transform filter text to lower case
-// if card title does not include the filter text
-// hide the card element
-// else
-// unhide the card element
-// else
-// for each card of cards
-// unhide the card element
+function filterCard(){
+    // if the filter is not empty
+    const valueFilter = filter.value
+    if(valueFilter != ''){
+        // for each card of cards
+        for (let card of cards){
+            // get card heading (title)
+            let title = card.querySelector('h2')
+            // tranform to lower case
+            title = title.textContent.toLowerCase()
+            const filterText = filter.value.toLowerCase()
+            // if card title does not include the filter text
+            if(!title.includes(filterText)){
+                card.style.display = 'none'
+            }else{
+                card.style.display = "block";
+            }
+        }
+    }else{
+        for ( let card of cards){
+            card.style.display = 'block'
+        }
+    }
+}
